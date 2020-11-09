ecb66e8d0e33604d49de74cdd232856f14cf97cd
jacobzaidi <jacob@zaidi.id.ua>
2020-11-09 13:31:23 +1300

    CORS

--- src/main/java/com/zaidi/trees/controllers/TreeApiController.java
+++ src/main/java/com/zaidi/trees/controllers/TreeApiController.java
@@ -6,6 +6,7 @@ import com.zaidi.trees.services.TreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/")
 public class TreeApiController {
